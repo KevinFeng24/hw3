66cf0c4363984c85819f510beef243e0cc0e6021
KevinFeng24 <43217317+KevinFeng24@users.noreply.github.com>
2018-09-26 21:13:39 -0700

    Create 15-drops-dripping.js

---
+++ 15-drops-dripping.js
@@ -0,0 +1,28 @@
+
+var x = 230;
+var y = [220,250,280,310,340,370,400,430,460,490,520,550,580,610,640];
+
+function setup() {
+  createCanvas(400, 400);
+}
+  
+function draw() {
+  background(0);
+  noStroke();
+
+  // draw pipe
+  rect(0, 200, x, 20);
+  
+  for (var i = 0; i < y.length; i = i + 1) {
+    // draw drip
+    ellipse(x, y[i], 10);
+
+    // down 3 pixels each frame
+    y[i] = y[i] + 3;
+
+    // if invisible for a full “height” amount, reset
+    if (y[i] > height*2) {
+      y[i] = 220;
+    }
+  }
+}
